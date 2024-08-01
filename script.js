74241592c6105e1c109c27a5dbc957e04496ae0e
vivek khamkar <khamkarv01@gmail.com>
2024-08-01 22:27:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const moon = document.querySelector(".moon");
 dark.addEventListener("click", ()=> {
     document.body.classList.toggle("dark");
     moon.classList.toggle("fa-moon");
-    moon.innerText.toggle("Light Mode");
+    dark.innerText.toggle("Light Mode");
 })
 
 select.addEventListener("click", ()=> {
